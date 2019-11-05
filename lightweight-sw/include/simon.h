e53ca886974bd9cf3699b138cb50e68bdf127369
Bruno Morais <brunosmmm@gmail.com>
2019-11-05 14:35:36 -0500

    skeleton code test

--- lightweight-sw/include/simon.h
+++ lightweight-sw/include/simon.h
@@ -1,4 +1,13 @@
-#ifndef _SIMON_H_INCLUDE_
+#ifndef _SIMON_H_INCLUDED_
 #define _SIMON_H_INCLUDED_
+
+// Simon 128 implementation
+#include "lightweight.h"
+
+void simon128_encrypt(key128_t key, block128_t *input, block128_t *output,
+                      uint32_t block_count);
+
+void simon128_decrypt(key128_t key, block128_t *input, block128_t *output,
+                      uint32_t block_count);
 
 #endif
--- lightweight-sw/src/simon.c
+++ lightweight-sw/src/simon.c
@@ -1,1 +1,22 @@
 #include "simon.h"
+
+static void _simon128_enc_round(key128_t round_key,
+                                block128_t input, block128_t* output) {
+
+}
+
+static void _simon128_dec_round(key128_t round_key, block128_t input,
+                                block128_t *output) {
+}
+
+void simon128_encrypt(key128_t key,
+                      block128_t* input, block128_t* output,
+                      uint32_t block_count) {
+
+}
+
+void simon128_decrypt(key128_t key,
+                      block128_t* input, block128_t* output,
+                      uint32_t block_count) {
+
+}
