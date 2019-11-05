d402300e1ffc8c7bb51b661fcc8757235abc8e68
Bruno Morais <brunosmmm@gmail.com>
2019-11-05 15:06:07 -0500

    utilities

---
+++ lightweight-sw/tests/util.c
@@ -0,0 +1,26 @@
+#include "util.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int util_get_rand(uint32_t count, uint8_t* dest) {
+  FILE* fp = NULL;
+  int ret = 0;
+  if (!dest || count) {
+    return ERR_VALUE;
+  }
+
+  fp = fopen("/dev/urandom", "rb");
+  if (!fp) {
+    return ERR_OS;
+  }
+
+  ret = fread(dest, 1, count, fp);
+  if (ret != count) {
+    fclose(fp);
+    return ERR_OS;
+  }
+
+  fclose(fp);
+  return ERR_OK;
+}
---
+++ lightweight-sw/tests/util.h
@@ -0,0 +1,14 @@
+#ifndef _UTIL_H_INCLUDED_
+#define _UTIL_H_INCLUDED_
+
+#include "lightweight.h"
+#include <stdio.h>
+
+#define ERR_OK 0 // no error
+#define ERR_VALUE 1 // invalid value passed
+#define ERR_OS 2 // OS error
+
+// get random bytes
+int util_get_rand(uint32_t count, uint8_t *dest);
+
+#endif
