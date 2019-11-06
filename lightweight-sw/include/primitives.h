6b79d705cc1b8ddf53f4b198bb04f2ec1bc17be6
Bruno Morais <brunosmmm@gmail.com>
2019-11-05 20:23:42 -0500

    add modular add/sub

--- lightweight-sw/include/primitives.h
+++ lightweight-sw/include/primitives.h
@@ -5,5 +5,9 @@
   ((x << amount) | (x >> ((dsize * 8) - amount)))
 #define ROTATE_RIGHT(x, amount, dsize)                                         \
   ((x >> amount) | ((x & ((1 << amount) - 1) << ((dsize * 8) - amount))))
+#define MOD_ADD(x, y, n)                        \
+  ((x + y)%(1<<n))
+#define MOD_SUB(x, y, n)                        \
+  ((x - y) % (1 << n))
 
 #endif
