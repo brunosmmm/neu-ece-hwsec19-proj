afdfc9df71b14210daf4f958d0ce8aa4ec2bee85
Bruno Morais <brunosmmm@gmail.com>
2019-11-07 15:37:28 -0500

    best try at running a bare metal program in spike. doesnt work

--- lightweight-sw/tests/test_simon_mmio.c
+++ lightweight-sw/tests/test_simon_mmio.c
@@ -1,6 +1,6 @@
 // Test Simon RoCC
 #include <stdint.h>
-#include <stdio.h>
+// #include <stdio.h>
 #include "xcustom.h"
 #include "riscv/rvutil.h"
 
@@ -34,9 +34,9 @@ static uint64_t regread(uint64_t* base, uint64_t offset) {
 }
 
 // A 128-bit key
-const static uint8_t test_key[16] = {0x80, 0x01, 0x02, 0xFF, 0x2A, 0xAA,
-                                     0x42, 0x00, 0x11, 0x30, 0xF9, 0xA4,
-                                     0xBB, 0x09, 0xAB, 0x56};
+const uint8_t test_key[16] = {0x80, 0x01, 0x02, 0xFF, 0x2A, 0xAA,
+                              0x42, 0x00, 0x11, 0x30, 0xF9, 0xA4,
+                              0xBB, 0x09, 0xAB, 0x56};
 
 // a 64-bit plaintext block
 static uint8_t test_block[8] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
@@ -44,7 +44,7 @@ static uint8_t test_block[8] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
 
 int main(void) {
   uint64_t rounds = 0, kw1 = 0, kw2 = 0, block = 0, cipher = 0;
-  uint64_t cycles = 0, time = 0, instret = 0;
+  uint64_t cycles = 0;
   unsigned int i = 0;
 
   // arrange key words from array
@@ -77,17 +77,17 @@ int main(void) {
 
   cycles = rv64_get_cycles() - cycles;
 
-  printf("INFO: ciphertext is 0x%lx\n", cipher);
+  // printf("INFO: ciphertext is 0x%lx\n", cipher);
 
   // verify operation
   if (*((uint64_t*)test_block) != block) {
     // fail
-    printf("ERROR: validation failed\n");
-    printf("expected: 0x%lx, got 0x%lx\n", *((uint64_t*)test_block), block);
+    // printf("ERROR: validation failed\n");
+    // printf("expected: 0x%lx, got 0x%lx\n", *((uint64_t*)test_block), block);
     return 1;
   }
 
-  printf("INFO: test succeeded. Cycles = %lu\n", cycles);
+  // printf("INFO: test succeeded. Cycles = %lu\n", cycles);
 
   return 0;
 }
