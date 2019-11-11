91becc03ca3668fcc6a876415a9b9a8741220597
Bruno Morais <brunosmmm@gmail.com>
2019-11-11 12:43:47 -0500

    fix bad rotate right macro

--- lightweight-sw/include/primitives.h
+++ lightweight-sw/include/primitives.h
@@ -4,7 +4,7 @@
 #define ROTATE_LEFT(x, amount, dsize)                                          \
   ((x << amount) | (x >> ((dsize * 8) - amount)))
 #define ROTATE_RIGHT(x, amount, dsize)                                         \
-  ((x >> amount) | ((x & ((1 << amount) - 1) << ((dsize * 8) - amount))))
+  ((x >> amount) | ((x & ((1 << amount) - 1)) << ((dsize * 8) - amount)))
 #define MOD_ADD(x, y, n)                        \
   ((x + y)%(1<<n))
 #define MOD_SUB(x, y, n)                        \
