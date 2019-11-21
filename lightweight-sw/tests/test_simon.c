a3ddf04dc3800774078ef7177f933aa8550a868a
Bruno Morais <brunosmmm@gmail.com>
2019-11-21 12:41:43 -0500

    fix printf, add debug code

--- lightweight-sw/tests/test_simon.c
+++ lightweight-sw/tests/test_simon.c
@@ -9,6 +9,8 @@
 #include "util.h"
 #endif
 
+// #define DEBUG
+
 const static key128_t test_key = {0x80, 0x01, 0x02, 0xFF, 0x2A, 0xAA, 0x42, 0x00,
                                   0x11, 0x30, 0xF9, 0xA4, 0xBB, 0x09, 0xAB, 0x56};
 
@@ -17,6 +19,13 @@ static block64_t test_block = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
 int main(void) {
   unsigned int i = 0;
   block64_t tmp, out, cipher;
+
+#ifdef BAREMETAL
+#ifdef DEBUG
+  volatile int wait = 1;
+  while (wait);
+#endif
+#endif
 
 #ifdef RISCV64
   uint64_t rv64_cycles = rv64_get_cycles();
@@ -45,7 +54,7 @@ int main(void) {
   // display plaintext
   print_bytes((uint8_t*)out, sizeof(block64_t));
 #else
-  printf("INFO: ciphertext is 0x%lx\n", cipher);
+  printf("INFO: ciphertext is 0x%lx\n", *cipher);
 #endif
 
   // verify
