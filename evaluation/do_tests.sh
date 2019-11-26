bd271a71af2bc58101f4427f21421047cbf8cf98
Bruno Morais <brunosmmm@gmail.com>
2019-11-26 14:31:20 -0500

    implement some ugly hacks in the evaluation script

--- evaluation/do_tests.sh
+++ evaluation/do_tests.sh
@@ -3,7 +3,10 @@
 BOOM_SIM=simulator-example-SimonBoomConfig
 ROCKET_SIM=simulator-example-SimonRocketConfig
 
-for test in $@; do
-    ./$BOOM_SIM $test > ${test}.result_boom &
-    ./$ROCKET_SIM $test > ${test}.result_rocket &
+# from stack overflow
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+for test in "${@:2}"; do
+    ./$BOOM_SIM $DIR/$test > ${test}.result_boom &
+    ./$ROCKET_SIM $DIR/$test > ${test}.result_rocket &
 done
