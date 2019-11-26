c0c4d7dbc16e7a3637bc2c87d7de483a55a4481b
Bruno Morais <brunosmmm@gmail.com>
2019-11-26 16:48:02 -0500

    use bare test code

--- evaluation/src/evaluate8.c
+++ evaluation/src/evaluate8.c
@@ -2,9 +2,23 @@
 #include "toosly_tests.h"
 
 void do_test(void) {
+  uint64_t kw1, kw2;
   TestResult result;
+  kw1 = ((uint64_t *)key)[0];
+  kw2 = ((uint64_t *)key)[1];
+
   printf("Testing ECB mode with toosly acc...");
-  result = test_toosly_ecb();
+  // initialize
+  TOOSLY_INIT(kw1, kw2);
+
+  // load test block
+  // block = *((uint64_t *)test_block);
+  test_start(&result);
+  // perform encryption rounds
+  TOOSLY_ENC(&testData, TEST_DATA_SIZE);
+
+  asm volatile("fence");
+  test_end(&result);
   printf(" done in %lu cycles, %lu instructions\n", result.cycles,
          result.instret);
 }
