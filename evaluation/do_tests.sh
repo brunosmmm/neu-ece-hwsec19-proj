2e16bdbc5e669db445b222a9061eb1a0d5c9b456
Bruno Morais <brunosmmm@gmail.com>
2019-11-26 14:42:30 -0500

    fix name, clean results

--- evaluation/do_tests.sh
+++ evaluation/do_tests.sh
@@ -4,7 +4,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 for test in "${@:2}"; do
-    echo "Launching evaluation $evaluation"
+    echo "Launching evaluation $test"
     $BOOM_SIM $DIR/$test 2>/dev/null 1> ${test}.result_boom &
     $ROCKET_SIM $DIR/$test 2>/dev/null 1> ${test}.result_rocket &
 done
