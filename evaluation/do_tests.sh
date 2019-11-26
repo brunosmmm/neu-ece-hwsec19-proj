c596f0aa6047f9df0060bef1fca8476d89b25eb4
Bruno Morais <brunosmmm@gmail.com>
2019-11-26 15:24:11 -0500

    aggregate results

--- evaluation/do_tests.sh
+++ evaluation/do_tests.sh
@@ -8,3 +8,15 @@ for test in "${@:1}"; do
     $BOOM_SIM $DIR/$test 2>/dev/null 1> ${test}.result_boom &
     $ROCKET_SIM $DIR/$test 2>/dev/null 1> ${test}.result_rocket &
 done
+
+# wait for completion
+echo "Waiting for completion..."
+wait
+
+rm -rf boom.results rocket.results
+for test in "${@:1}"; do
+    sed -r 's/[^0-9]*([0-9]+)\s+cycles$/\1/' $test.result_boom >> boom.results
+    sed -r 's/[^0-9]*([0-9]+)\s+cycles$/\1/' $test.result_rocket >> rocket.results
+done
+
+echo "Done"
