14af86abbaab7f1c489f6e4b33bc05e4d66167d3
Bruno Morais <brunosmmm@gmail.com>
2019-11-26 14:40:08 -0500

    update a bit

--- evaluation/do_tests.sh
+++ evaluation/do_tests.sh
@@ -4,6 +4,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 for test in "${@:2}"; do
-    $BOOM_SIM $DIR/$test > ${test}.result_boom &
-    $ROCKET_SIM $DIR/$test > ${test}.result_rocket &
+    echo "Launching evaluation $evaluation"
+    $BOOM_SIM $DIR/$test 2>/dev/null 1> ${test}.result_boom &
+    $ROCKET_SIM $DIR/$test 2>/dev/null 1> ${test}.result_rocket &
 done
