f76d5a3c637c940e7884becd4a48a1c9da0d4864
Bruno Morais <brunosmmm@gmail.com>
2019-11-26 14:35:55 -0500

    dont define executable path

--- evaluation/do_tests.sh
+++ evaluation/do_tests.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-BOOM_SIM=simulator-example-SimonBoomConfig
-ROCKET_SIM=simulator-example-SimonRocketConfig
-
 # from stack overflow
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 for test in "${@:2}"; do
-    ./$BOOM_SIM $DIR/$test > ${test}.result_boom &
-    ./$ROCKET_SIM $DIR/$test > ${test}.result_rocket &
+    $BOOM_SIM $DIR/$test > ${test}.result_boom &
+    $ROCKET_SIM $DIR/$test > ${test}.result_rocket &
 done
