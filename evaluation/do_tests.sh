b19d7c3be3f23724a026369ea372be94aaef4517
Bruno Morais <brunosmmm@gmail.com>
2019-11-26 14:43:32 -0500

    unsure why 2 was there

--- evaluation/do_tests.sh
+++ evaluation/do_tests.sh
@@ -3,8 +3,8 @@
 # from stack overflow
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-for test in "${@:2}"; do
-    echo "Launching evaluation $test"
+for test in "${@:1}"; do
+    echo "Launching $test"
     $BOOM_SIM $DIR/$test 2>/dev/null 1> ${test}.result_boom &
     $ROCKET_SIM $DIR/$test 2>/dev/null 1> ${test}.result_rocket &
 done
