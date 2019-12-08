37774311888d205a1355469857b21bffa56bf878
Bruno Morais <brunosmmm@gmail.com>
2019-12-07 19:38:09 -0500

    update plot script, blackify

--- reports/media/plots.py
+++ reports/media/plots.py
@@ -1,12 +1,20 @@
 """Generate plots for results."""
 import matplotlib
+
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy
 
-TEST_NAMES = ["SW/ECB", "SW/CBC", "MMIO/ECB (automatic)",
-              "MMIO/ECB (interactive)", "MMIO/CBC", "RoCC/ECB",
-              "RoCC/CBC", "Hybrid/EBC"]
+TEST_NAMES = [
+    "SW/ECB",
+    "SW/CBC",
+    "MMIO/ECB (automatic)",
+    "MMIO/ECB (interactive)",
+    "MMIO/CBC",
+    "RoCC/ECB",
+    "RoCC/CBC",
+    "Hybrid/EBC",
+]
 
 ROCKET_RESULTS = "../../evaluation/rocket.results"
 BOOM_RESULTS = "../../evaluation/boom.results"
@@ -18,7 +26,7 @@ def parse_results(data):
 
 
 if __name__ == "__main__":
-    plt.style.use('bmh')
+    plt.style.use("bmh")
 
     # load results
     with open(ROCKET_RESULTS, "r") as rocketdata:
@@ -42,43 +50,70 @@ if __name__ == "__main__":
     # horizontal bar plots
     # plot 1 is ECB mode cycle count normalized to Rocket
     fig, ax = plt.subplots()
-    ecb_benchmarks = ["2-way\nBOOM", "MMIO\n(auto)", "MMIO\n(single)",
-                      "RoCC", "Hybrid\n(batch)"]
-    ecb_cycles = [boom_cycles[0], rocket_cycles[2],
-                  rocket_cycles[3], rocket_cycles[5], rocket_cycles[7]]
-    ecb_speedup = [slowest_ebc/cycles for cycles in ecb_cycles]
+    ecb_benchmarks = [
+        "2-way\nBOOM",
+        "MMIO\n(auto)",
+        "MMIO\n(single)",
+        "RoCC",
+        "Hybrid\n(batch)",
+    ]
+    ecb_cycles = [
+        boom_cycles[0],
+        rocket_cycles[2],
+        rocket_cycles[3],
+        rocket_cycles[5],
+        rocket_cycles[7],
+    ]
+    ecb_speedup = [slowest_ebc / cycles for cycles in ecb_cycles]
 
     ecb_data_sorted = sorted(zip(ecb_speedup, ecb_benchmarks))
     ecb_sorted_names = [x for _, x in ecb_data_sorted]
     ecb_sorted_speedup = [x for x, _ in ecb_data_sorted]
 
     y_pos = numpy.arange(len(ecb_benchmarks))
-    ax.barh(y_pos, ecb_sorted_speedup, align="center", color="steelblue", height=0.5, edgecolor="black")
+    ax.barh(
+        y_pos,
+        ecb_sorted_speedup,
+        align="center",
+        color="steelblue",
+        height=0.5,
+        edgecolor="black",
+    )
     ax.set_yticks(y_pos)
-    ax.set_xticks(numpy.arange(0, 5, 0.5))
+    ax.set_xticks(numpy.arange(0, 6.5, 0.5))
     ax.set_yticklabels(ecb_sorted_names)
     ax.set_xlabel("Speedup")
-    ax.set_title("Speedup compared to pure software in-order implementation\n(ECB mode)")
+    ax.set_title(
+        "Speedup compared to pure software in-order implementation\n(ECB mode)"
+    )
     ax.grid(axis="y", b=False)
 
     plt.savefig("ecb_speedup.pdf")
 
     fig, ax = plt.subplots()
     cbc_benchmarks = ["2-way\nBOOM", "MMIO\n(single)", "RoCC"]
-    cbc_cycles = [boom_cycles[1], rocket_cycles[4],
-                  rocket_cycles[6]]
-    cbc_speedup = [slowest_cbc/cycles for cycles in cbc_cycles]
+    cbc_cycles = [boom_cycles[1], rocket_cycles[4], rocket_cycles[6]]
+    cbc_speedup = [slowest_cbc / cycles for cycles in cbc_cycles]
 
     cbc_data_sorted = sorted(zip(cbc_speedup, cbc_benchmarks))
     cbc_sorted_names = [x for _, x in cbc_data_sorted]
     cbc_sorted_speedup = [x for x, _ in cbc_data_sorted]
 
     y_pos = numpy.arange(len(cbc_benchmarks))
-    ax.barh(y_pos, cbc_sorted_speedup, align="center", color="steelblue", height=0.5, edgecolor="black")
+    ax.barh(
+        y_pos,
+        cbc_sorted_speedup,
+        align="center",
+        color="steelblue",
+        height=0.5,
+        edgecolor="black",
+    )
     ax.set_yticks(y_pos)
     ax.set_yticklabels(cbc_sorted_names)
     ax.set_xlabel("Speedup")
-    ax.set_title("Speedup compared to pure software in-order implementation\n(CBC mode)")
+    ax.set_title(
+        "Speedup compared to pure software in-order implementation\n(CBC mode)"
+    )
     ax.grid(axis="y", b=False)
 
     plt.savefig("cbc_speedup.pdf")
